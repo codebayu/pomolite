e3ca3ab8df09e566efc464591b1c2b2a2ef88e74
Bayu Setiawan <bayusetiawanipb@gmail.com>
2023-08-08 12:27:42 +0700

    setup prisma and server fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
+import { ITask } from '@/common/types/task';
+import axios, { AxiosResponse } from 'axios';
+
+export default async function Home() {
+  const { data: tasks } = await getTask();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World!</h1>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h2>{task.title}</h2>
+          <p>{task.content}</p>
+        </div>
+      ))}
     </main>
   );
+}
+
+// Server Fetching
+async function getTask(): Promise<{ status: number; data: ITask[] }> {
+  const response: AxiosResponse = await axios.get(
+    `${process.env.BASE_URL}/api/tasks`
+  );
+  return {
+    status: response.status,
+    data: response.data.data,
+  };
 }
