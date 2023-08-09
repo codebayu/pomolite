42e8e39d5d495ca504baa22594f36cbdf67f6c9c
Bayu Setiawan <bayusetiawanipb@gmail.com>
2023-08-09 18:40:09 +0700

    update crud logic base new model

--- modules/home/components/TaskFormCollapse.tsx
+++ modules/home/components/TaskFormCollapse.tsx
@@ -23,7 +23,7 @@ export default function TaskFormCollapse(props: TaskFormCollapseProps) {
 
   async function handleCreate(e: FormEvent) {
     e.preventDefault();
-    await axios.post('/api/task', { title, content, userId: 1 });
+    await axios.post('/api/task', { title, content });
     closeCollapse();
     router.refresh();
     setTitle('');
